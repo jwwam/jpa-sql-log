8c9eef505373d223de8cae3dd785a98646c4ad18
824247231@qq.com <zyq824247231>
2020-07-29 16:47:32 +0800

    更新日志

--- src/main/java/cn/feelcode/jpa/JpaSqlLogAction.java
+++ src/main/java/cn/feelcode/jpa/JpaSqlLogAction.java
@@ -1,16 +1,9 @@
 package cn.feelcode.jpa;
 
-
-import com.intellij.execution.filters.TextConsoleBuilderFactory;
-import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.pom.Navigatable;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -33,10 +26,6 @@ public class JpaSqlLogAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent event) {
 
         ToolWindowManager.getInstance(event.getProject()).getToolWindow("JpaSqlLog").show();
-
-        //ActionManager.createActionToolbar();
-        //TextConsoleBuilderFactory.getInstance().createBuilder(event.getProject()).getConsole().;
-
         //ToolWindow toolWindow = ToolWindowManager.getInstance(event.getProject()).getToolWindow("JpaSqlLog");
         //new JpaToolWindow(event.getProject(),toolWindow);
     }
